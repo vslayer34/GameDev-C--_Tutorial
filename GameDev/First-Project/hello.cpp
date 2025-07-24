a54e2555bedae0f2b6abbabc2e58946f9e4c8f4f
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2025-07-25 01:24:02 +0300

    Added project files and 2.5 Comparison Operators

---
+++ GameDev/First-Project/hello.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <iostream>
+
+/*
+* Auther:
+* Mohamed El-Nagar
+*/
+
+
+/// @brief Self introtuction program
+/// @return 
+int main()
+{
+    // Print name
+    printf("Hello, My name is Mohamed\n");
+
+    // print my home town
+    printf("I'm born in Damanhour\n");
+
+    // print my favorite hobby
+    printf("I'm a gamer\n");
+}
