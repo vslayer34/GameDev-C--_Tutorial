679219dbc5ee9dbd027802c6bffe95083102a9f6
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2025-07-25 18:39:19 +0300

    Axe Game: 2.9 Drawing A Circle

--- Axe-Game/main.cpp
+++ Axe-Game/main.cpp
@@ -4,12 +4,18 @@
 int main()
 {
     int width { 800 };
-    InitWindow(width, 600, "Test");
+    int height { 600 };
+    InitWindow(width, height, "Test");
+
+    int centerX { width / 2 };
+    int centerY { height / 2 };
+    float radius { 25.0f };
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(RED);
+        ClearBackground(WHITE);
+        DrawCircle(centerX, centerY, radius, BLUE);
         EndDrawing();
     }
     
