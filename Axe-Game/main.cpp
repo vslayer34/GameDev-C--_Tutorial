fa7f7ab5ad4f1972c94c2d3d4f73634c3bb36b31
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2025-07-25 18:54:03 +0300

    Axe Game: 2.10 Moving The Circle
    Moved the circle left and right using the A & D inputs

--- Axe-Game/main.cpp
+++ Axe-Game/main.cpp
@@ -3,10 +3,13 @@
 
 int main()
 {
+    // Window Configs
     int width { 800 };
     int height { 600 };
     InitWindow(width, height, "Test");
+    SetTargetFPS(60);
 
+    // Circle Coordiants
     int centerX { width / 2 };
     int centerY { height / 2 };
     float radius { 25.0f };
@@ -15,7 +18,19 @@ int main()
     {
         BeginDrawing();
         ClearBackground(WHITE);
+        // Start Game Logic
         DrawCircle(centerX, centerY, radius, BLUE);
+
+        if (IsKeyDown(KEY_D))
+        {
+            centerX = centerX + 5;
+        }
+        if (IsKeyDown(KEY_A))
+        {
+            centerX = centerX - 5;
+        }
+
+        // Start Game Logic
         EndDrawing();
     }
     
