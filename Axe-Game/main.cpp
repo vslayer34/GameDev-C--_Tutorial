5f4c0ca15e308d8df8ffecd0c5184c32f802e0b7
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2025-07-25 18:04:16 +0300

    Axe Game: 2.6 Loops

--- Axe-Game/main.cpp
+++ Axe-Game/main.cpp
@@ -3,17 +3,14 @@
 
 int main()
 {
-    bool equal { 4 == 9 };                  // false
-    bool notEqual { 4 != 9 };               // true
-
-    bool less { 4 < 9 };                    // true
-    bool greater { 4 > 9 };                 // false
-
-    bool lessEqual { 4 <= 9 };              // true
-    bool greaterEqual { 4 >= 9 };           // false
-
     int width { 800 };
     InitWindow(width, 600, "Test");
+
+    while (true)
+    {
+        /* code */
+    }
+    
 
     return 0;
 }
