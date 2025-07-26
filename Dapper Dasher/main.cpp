82508425df52b1355d2d34ca0ac529b8a493a9e1
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2025-07-26 21:40:52 +0300

    Dapper Dasher: 3.2 Setting Up main.cpp
    Setup the raylib window

--- Dapper Dasher/main.cpp
+++ Dapper Dasher/main.cpp
@@ -1,5 +1,26 @@
+#include "raylib.h"
+
 int main()
 {
+    // Window setup
+    const int WINDOW_WIDTH { 800 };
+    const int WINDOW_HEIGHT { 600 };
+    SetTargetFPS(60);
+
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Dapper Dasher");
+
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(WHITE);
+        // Begin Game Logic
+        
+        
+        // End Game Logic
+        EndDrawing();
+    }
+    CloseWindow();
 
     return 0;
 }
