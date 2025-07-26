5025ca7c962ffea432fa5c66da88ff3c35157fe1
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2025-07-26 22:08:19 +0300

    Dapper Dasher: 3.3 Velocity and Jumping
    Added Jumping to a test rectangle

--- Dapper Dasher/main.cpp
+++ Dapper Dasher/main.cpp
@@ -9,12 +9,27 @@ int main()
 
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Dapper Dasher");
 
+    // Test rectangle
+    const int RECT_WIDTH { 20 };
+    const int RECT_HEIGHT { 20 };
+    int rectPosY { WINDOW_HEIGHT - RECT_HEIGHT };
+    int rectPosX { WINDOW_WIDTH / 2 };
+
+    const int JUMP_VELOCITY { -5 };
+
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(WHITE);
         // Begin Game Logic
+
+        DrawRectangle(rectPosX, rectPosY, RECT_WIDTH, RECT_HEIGHT, RED);
+
+        if (IsKeyPressed(KEY_SPACE))
+        {
+            rectPosY += JUMP_VELOCITY;
+        }
         
         
         // End Game Logic
